8e3697d71830de9f7d1b27ce1da9a4f8af94a4d7
stewardhuaman25 <stewardsherillhuaman@gmail.com>
2023-04-19 09:43:58 -0500

    subi mi primera plantilla jajjaja

---
+++ footer.php
@@ -0,0 +1,7 @@
+<div class=" container">
+    <div class="row">
+        <div class="col-md-12">
+            <p>&copy; STEWARD HUAMAN HUAMAN</p>
+        </div>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Steward</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+ <head>
+  <?php 
+  require_once('header.php');
+  ?>
+ </head>
+
+  <div class="container">
+    <h1> GALERIA</h1>
+    <div class="row">
+
+      <div class="col-lg-4 col-md-6 col-sm-12 ">
+        <div class="card">
+          <img src="img/istockphoto-843408508-170667a.jpg" alt="Imagen 1" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">zasasasass</h5>
+            <p class="card-text">Cuéntamelo y me olvidaré. enséñamelo y lo recordaré. involúcrame y lo aprenderé -
+              Benjamin Franklin </p>
+            <a href="#" class="btn btn-primary">Comprar</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 col-sm-12 ">
+        <div class="card">
+          <img src="img/girasoles-978x652.jpg" alt="Imagen 2" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">asasas</h5>
+            <p class="card-text">Cuéntamelo y me olvidaré. enséñamelo y lo recordaré. involúcrame y lo aprenderé -
+              Benjamin Franklin </p>
+            <a href="#" class="btn btn-primary">Comprar</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 col-sm-12 ">
+        <div class="card">
+          <img src="img/istockphoto-843408508-170667a.jpg" alt="Imagen 3" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">ojon</h5>
+            <p class="card-text">Cuéntamelo y me olvidaré. enséñamelo y lo recordaré. involúcrame y lo aprenderé -
+              Benjamin Franklin </p>
+            <a href="#" class="btn btn-primary">Comprar</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+
+      <div class="col-lg-4 col-md-6 col-sm-12 ">
+        <div class="card">
+          <img src="img/depositphotos_6030768-stock-illustration-romantic-hearts-valentines-day-design.jpg"
+            alt="Imagen 4" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">buhoooo</h5>
+            <p class="card-text">Cuéntamelo y me olvidaré. enséñamelo y lo recordaré. involúcrame y lo aprenderé -
+              Benjamin Franklin </p>
+            <a href="#" class="btn btn-primary">Comprar</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 col-sm-12 ">
+        <div class="card">
+          <img src="img/bancos_de_imagenes_gratis.webp" alt="Imagen 5" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">buho4</h5>
+            <p class="card-text">Cuéntamelo y me olvidaré. enséñamelo y lo recordaré. involúcrame y lo aprenderé -
+              Benjamin Franklin </p>
+            <a href="#" class="btn btn-primary">Comprar</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 col-sm-12 ">
+        <div class="card">
+          <img src="img/01-hubble-heic1501a.jpg" alt="Imagen 6" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">zbuho</h5>
+            <p class="card-text">Cuéntamelo y me olvidaré. enséñamelo y lo recordaré. involúcrame y lo aprenderé -
+              Benjamin Franklin </p>
+            <a href="#" class="btn btn-primary">Comprar</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <footer class="bg-dark text-white text-center py-3" ">
+    <?php 
+    require_once('footer.php');
+    ?>
+  </footer>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
